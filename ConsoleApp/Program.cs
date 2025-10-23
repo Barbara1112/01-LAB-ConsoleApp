7b3c8055a063b7ae7e6b8a2905c14427e5f7f71a
barbara <barbaramarkic2003@gmail.com>
2025-10-23 15:44:01 +0200

    ispravak lab1.

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,22 +1,22 @@
 ﻿namespace ConsoleApp
 {
-
-    public class Proizvod
+    internal class Program
     {
-        public string Naziv { get; set; }
-        public decimal Cijena { get; set; }
-        public int DostupnaKolicina { get; set; }
-        public Proizvod (string naziv, decimal cijena, int dostupnaKolicina)
+        static void Main(string[] args)
         {
-            Naziv = naziv;
-            Cijena = cijena;
-            DostupnaKolicina = dostupnaKolicina;
-        }
+            // 1. zadatak – Proizvod
+            Proizvod p = new Proizvod("Laptop", 999.99m, 5);
+            p.IspisiInformacije();
 
-        public void PrikazInformacije ()
-        {
-            Console.WriteLine($"Naziv: {Naziv}, Cijena: {Cijena} Eura, Dostupna količina {DostupnaKolicina}");
+            Console.WriteLine("\n-----------------------------\n");
+
+            // 2. zadatak – Nasljeđivanje
+            PametniTelefon telefon = new PametniTelefon("Galaxy S24", "Samsung", 2024, "Android");
+            telefon.IspisiInformacije();
+            telefon.InstalirajAplikaciju("WhatsApp");
+
+            Console.WriteLine("\n--- Pritisni Enter za kraj ---");
+            Console.ReadLine();
         }
     }
-}
 }
---
+++ ConsoleApp/pametnitelefon.cs
@@ -0,0 +1,24 @@
+﻿namespace ConsoleApp
+{
+    public class PametniTelefon : ElektronickiUredjaj
+    {
+        public string OperativniSustav { get; set; }
+
+        public PametniTelefon(string naziv, string proizvodjac, int godinaProizvodnje, string operativniSustav)
+            : base(naziv, proizvodjac, godinaProizvodnje)
+        {
+            OperativniSustav = operativniSustav;
+        }
+
+        public void InstalirajAplikaciju(string nazivAplikacije)
+        {
+            Console.WriteLine($"Instalirana aplikacija: {nazivAplikacije}");
+        }
+
+        public new void IspisiInformacije()
+        {
+            base.IspisiInformacije();
+            Console.WriteLine($"Operativni sustav: {OperativniSustav}");
+        }
+    }
+}
